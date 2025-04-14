b4f50c884f19bb53cbc5981d116267eac333b5b5
scabbage <thescabbage@gmail.com>
2025-04-14 19:25:49 +0800

    Fix dumdum error again

--- lua/project_nvim/project.lua
+++ lua/project_nvim/project.lua
@@ -12,7 +12,7 @@ M.last_project = nil
 function M.find_lsp_root()
   -- Get lsp client for current buffer
   -- Returns nil or string
-  local buf_ft = vim.api.nvim_buf_get_option_value("filetype", { buf = 0 })
+  local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
   local clients = vim.lsp.get_clients({ buffer = 0 })
   if next(clients) == nil then
     return nil
